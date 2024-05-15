f7867afc1f1146b62aa8c134bc4ecd3224b68c60
Hans Gier <aurorasupreme9001@gmail.com>
2024-05-15 19:44:38 +0800

    edit 3

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -2,18 +2,9 @@ const router = require('express').Router();
 const { authenticateUser } = require('../middlewares/authentication');
 const { register, login, logout, forgotPassword, resetPassword } = require('../controllers/authController');
 
-const rateLimiter = require('express-rate-limit');
 
-const apiLimiter = rateLimiter({
-    windowMs: 5 * 60 * 1000,
-    max: 10,
-    message: {
-        msg: 'Too many requests, please try again after 5 minutes',
-    },
-});
-
-router.route('/register').post(apiLimiter, register);
-router.route('/login').post(apiLimiter, login);
+router.route('/register').post(register);
+router.route('/login').post(login);
 router.route('/logout').delete(authenticateUser, logout);
 router.route('/forgot-password').post(forgotPassword);
 router.route('/reset-password').post(resetPassword);
