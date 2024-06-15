433269bd640c3775b9598a54b7a28c0da1826ee6
Hans Gier <aurorasupreme9001@gmail.com>
2024-06-15 15:25:15 +0800

    edited redis

--- config/redis.js
+++ config/redis.js
@@ -1,5 +1,9 @@
 const Redis = require("ioredis");
-
-const redis = new Redis();
+const redisURL = process.env.REDIS_TLS_URL;
+const redis = new Redis(redisURL, {
+    tls: {
+        rejectUnauthorized: false,
+    },
+});
 
 module.exports = redis;
